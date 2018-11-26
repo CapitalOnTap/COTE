1e884e5a7d1932e5f569cfeb3762c9a6b492358e
Andriy <chapran66@gmail.com>
2018-11-26 13:02:19 +0200

    Add control of active tab from outside Tabs component

--- src/components/Tabs/Tabs.stories.js
+++ src/components/Tabs/Tabs.stories.js
@@ -1,11 +1,12 @@
 import { storiesOf } from '@storybook/react';
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
 import { Caption } from '../atoms/Typography';
 import TextInput from '../TextInput/TextInput';
 import WidgetBox from '../WidgetBox/WidgetBox';
 import Tab from './Tab';
 import Tabs from './Tabs';
+import Button from '../atoms/Button/Button'
 
 const ButtonWrapper = styled.div`
   padding: 1rem;
@@ -37,34 +38,37 @@ class MyComponent extends Component {
 
   render() {
     return (
-      <WidgetBox>
-        <Tabs onTabClicked={this.handleTabClicked} activeIndex={this.state.selectedTab}>
-          <Tab title="Redeem cash">
-            <InputWrapper>
-              <TextInput
-                full
-                error="*The amount of money can’t surpass your redeemable points."
-              />
-            </InputWrapper>
-            <InputWrapper>
-              <Caption text="Conversion rate: 0.5%" />
-            </InputWrapper>
-            <InputWrapper>
-              <Caption text="Redeem fee: Free" />
-            </InputWrapper>
-            <InputWrapper>
-              <TextInput
-                error="*The amount of money can’t surpass your redeemable points."
-                full
-              />
-            </InputWrapper>
-          </Tab>
-          <Tab title="Redeem balance">
-            <h1>Panel 2 content</h1>
-          </Tab>
-          <Tab title="Third tab">this my third tab</Tab>
-        </Tabs>
-      </WidgetBox>
+      <Fragment>
+        <WidgetBox>
+          <Tabs onTabClicked={this.handleTabClicked} activeIndex={this.state.selectedTab}>
+            <Tab title="Redeem cash">
+              <InputWrapper>
+                <TextInput
+                  full
+                  error="*The amount of money can’t surpass your redeemable points."
+                />
+              </InputWrapper>
+              <InputWrapper>
+                <Caption text="Conversion rate: 0.5%" />
+              </InputWrapper>
+              <InputWrapper>
+                <Caption text="Redeem fee: Free" />
+              </InputWrapper>
+              <InputWrapper>
+                <TextInput
+                  error="*The amount of money can’t surpass your redeemable points."
+                  full
+                />
+              </InputWrapper>
+            </Tab>
+            <Tab title="Redeem balance">
+              <h1>Panel 2 content</h1>
+            </Tab>
+            <Tab title="Third tab">this my third tab</Tab>
+          </Tabs>
+        </WidgetBox>
+        <Button onClick={() => this.setState({ selectedTab: 0 })}>Switch to first tab</Button>
+      </Fragment>
     );
   }
 }
