7ded2290e7e9f05001c42535c3f1ccad58270e63
Ricardo Abreu <rj.abreu89@gmail.com>
2019-01-03 10:00:23 +0000

    typescript config for storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,11 @@
 const path = require('path');
 const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
-
-module.exports = (baseConfig, env, config) => {
-  config.module.rules.push({
+module.exports = (baseConfig, env, defaultConfig) => {
+  defaultConfig.module.rules.push({
     test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve('awesome-typescript-loader')
-      },
-      {
-        loader: require.resolve('react-docgen-typescript-loader')
-      }
-    ]
+    loader: require.resolve('awesome-typescript-loader')
   });
-  config.resolve.extensions.push('.ts', '.tsx');
-  return config;
+  defaultConfig.plugins.push(new TSDocgenPlugin());
+  defaultConfig.resolve.extensions.push('.ts', '.tsx');
+  return defaultConfig;
 };
