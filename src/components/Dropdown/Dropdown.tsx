cd3c3d25c758ee752202db026432f4260a2fd2f6
Ellis Knight <ellis.knight@capitalontap.com>
2019-05-31 11:06:12 +0100

    fix the type

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,12 +1,12 @@
-import PropTypes from "prop-types";
-import React, { Component } from "react";
-import styled from "styled-components";
-import { colors as defaultColors } from "../../styles/defaults";
-import Icon from "../atoms/Icon/Icon";
-import { Caption, Label } from "../atoms/Typography/index";
-import OutsideAlerter from "../OutsideAlerter/OutsideAlerter";
-import ResultsList from "../ResultsList/ResultsList";
-import { Option } from "../types";
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { colors as defaultColors } from '../../styles/defaults';
+import Icon from '../atoms/Icon/Icon';
+import { Caption, Label } from '../atoms/Typography/index';
+import OutsideAlerter from '../OutsideAlerter/OutsideAlerter';
+import ResultsList from '../ResultsList/ResultsList';
+import { Option } from '../types';
 
 const ArrowIcon = styled(Icon)<React.HTMLAttributes<{}>>`
   position: absolute;
@@ -20,24 +20,23 @@ const ArrowIcon = styled(Icon)<React.HTMLAttributes<{}>>`
 
 interface WrapperProps extends React.HTMLAttributes<{}> {
   full?: boolean;
-  error?: boolean;
+  error?: string;
   disabled?: boolean;
 }
 
 const Wrapper = styled.div<WrapperProps>`
   position: relative;
   min-width: 66px;
-  width: ${props => (props.full ? "100%" : "232px")};
+  width: ${props => (props.full ? '100%' : '232px')};
   border: ${props => {
-    if (props.error && props.theme)
-      return `1px solid ${props.theme.colorDanger}`;
+    if (props.error && props.theme) return `1px solid ${props.theme.colorDanger}`;
     if (props.error) return `1px solid ${defaultColors.danger}`;
 
     return `1px solid ${defaultColors.default}`;
   }};
   border-radius: 4px;
   padding: 9px 16px;
-  cursor: ${props => (props.disabled ? "default" : "pointer")};
+  cursor: ${props => (props.disabled ? 'default' : 'pointer')};
   user-select: none;
   outline: none;
 `;
@@ -80,9 +79,7 @@ class Dropdown extends Component<Props, State> {
   handleOptionChange = (option: Option) => {
     this.setState(
       { selectedOption: option, isOpen: false },
-      () =>
-        this.props.handleValueChange &&
-        this.props.handleValueChange(option.value)
+      () => this.props.handleValueChange && this.props.handleValueChange(option.value)
     );
   };
 
@@ -114,19 +111,12 @@ class Dropdown extends Component<Props, State> {
     } = this.props;
 
     return (
-      <OutsideAlerter
-        handleClickOutsideElement={this.handleClickOutsideDropdown}
-        {...props}
-      >
-        {label && <Label required={error} text={label} />}
+      <OutsideAlerter handleClickOutsideElement={this.handleClickOutsideDropdown} {...props}>
+        {label && <Label required={!!error} text={label} />}
 
         <Wrapper error={error} full={full} id={id} {...this.props} tabIndex={0}>
           <Clickable onClick={this.handleClick} />
-          {selectedOption ? (
-            <Title>{selectedOption.title}</Title>
-          ) : (
-            <Title>{title}</Title>
-          )}
+          {selectedOption ? <Title>{selectedOption.title}</Title> : <Title>{title}</Title>}
           {!isOpen && !disabled && (
             <ArrowIcon name="keyboard_arrow_down" onClick={this.handleClick} />
           )}
@@ -155,11 +145,7 @@ class Dropdown extends Component<Props, State> {
   options: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.any.isRequired,
-      value: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number,
-        PropTypes.bool
-      ])
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool])
     })
   ),
   /** Callback to retrieve value of option selected */
@@ -171,29 +157,21 @@ class Dropdown extends Component<Props, State> {
   /** An item added at the end of the results list - can be used for "can't find my option" scenarios */
   lastOption: PropTypes.shape({
     title: PropTypes.any.isRequired,
-    value: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number,
-      PropTypes.bool
-    ])
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool])
   }),
   selectedOption: PropTypes.shape({
     title: PropTypes.any.isRequired,
-    value: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number,
-      PropTypes.bool
-    ])
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.bool])
   }),
   disabled: PropTypes.bool
 };
 
 (Dropdown as any).defaultProps = {
-  title: "Select an option",
-  options: [{ title: "Option 1", value: 1 }, { title: "Option 2", value: 2 }],
+  title: 'Select an option',
+  options: [{ title: 'Option 1', value: 1 }, { title: 'Option 2', value: 2 }],
   handleValueChange: value => console.log(value),
   full: false,
-  label: "",
+  label: '',
   lastOption: null,
   selectedOption: null,
   disabled: false,
