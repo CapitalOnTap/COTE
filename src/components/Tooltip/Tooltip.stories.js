c86b93cb9c3569ecda08dd8ee5d225ffc91758d6
Christian Rodriguez <christian.rodriguez@capitalontap.com>
2019-07-12 15:11:16 +0100

    update tooltip icon styles and export it

--- src/components/Tooltip/Tooltip.stories.js
+++ src/components/Tooltip/Tooltip.stories.js
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react';
 import Tooltip from './Tooltip';
 
 storiesOf('Tooltip', module)
-  .add('Default', () => <Tooltip/>)
-  .add('No label', () => <Tooltip withoutLabel/>)
+  .add('Default', () => <Tooltip />)
+  .add('No label', () => <Tooltip withoutLabel />);
--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Flex } from '@rebass/grid';
-import { Tooltip as CotTooltip }  from 'react-tippy';
-import "react-tippy/dist/tippy.css";
+import { Tooltip as CotTooltip } from 'react-tippy';
+import 'react-tippy/dist/tippy.css';
 import TooltipContent from '../TooltipContent/index';
 import Icon from '../atoms/Icon/Icon';
 
-const TooltipIcon = styled(Icon)`  
+const TooltipIcon = styled(Icon)`
   cursor: pointer;
   padding: 0 10px;
+  vertical-align: middle;
 `;
 
 interface Props {
@@ -20,48 +21,35 @@ interface Props {
   label?: string;
 }
 
-const Tooltip: React.SFC<Props> = ({ 
-  title, 
-  description, 
-  position,
-  withoutLabel,
-  label
- }) => {
+const Tooltip: React.SFC<Props> = ({ title, description, position, withoutLabel, label }) => {
   return (
-    <Flex alignItems='center'>
-      {!withoutLabel && (
-        label
-      )}
+    <Flex alignItems="center">
+      {!withoutLabel && label}
       <CotTooltip
         arrow
         inertia
-        theme='light'   
+        theme="light"
         position={position}
-        html={
-          <TooltipContent
-            title={title}
-            description={description}          
-          />
-        }
-      >       
-        <TooltipIcon name='info' />
+        html={<TooltipContent title={title} description={description} />}
+      >
+        <TooltipIcon name="info" />
       </CotTooltip>
     </Flex>
-  )
-}
+  );
+};
 
 (Tooltip as any).propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   position: PropTypes.string,
   label: PropTypes.string
-}
+};
 
 Tooltip.defaultProps = {
   title: 'This is some tooltip title.',
   description: 'This is some tooltip description content.',
   position: 'top',
   label: 'Tooltip label'
-}
+};
 
-export default Tooltip
+export default Tooltip;
