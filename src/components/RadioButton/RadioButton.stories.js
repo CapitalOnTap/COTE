f0bf347d32282518769a6ab88ad22936cc7fddad
christian-rodriguez-capontap <52253610+christian-rodriguez-capontap@users.noreply.github.com>
2019-07-15 11:10:41 +0100

    Christian/add new radio style (#131)
    * v0.14.17
    
    * 0.14.18
    
    * 0.15.0
    
    * 1.0.0
    
    * 1.0.1
    
    * fixed version conflict
    
    * 1.0.0
    
    * update version
    
    * add new radio button style
    
    * update version

--- src/components/RadioButton/RadioButton.stories.js
+++ src/components/RadioButton/RadioButton.stories.js
@@ -1,17 +1,25 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-import styled from "styled-components";
-import RadioButton from ".";
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import RadioButton from '.';
 
-const Container = styled.div`
-  max-width: 400px;
-  margin: 0 auto;
-`;
+const NormalStyles = ({ children }) => (
+  <div style={{ maxWidth: '400px', margin: '0 auto' }}>{children}</div>
+);
 
-storiesOf("RadioButton", module).add("Normal", () => (
-  <Container>
+const InlineStyles = ({ children }) => (
+  <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>{children}</div>
+);
+
+storiesOf('RadioButton', module).add('Normal', () => (
+  <NormalStyles>
     <RadioButton value={1} label="Option 1" checked />
     <RadioButton value={2} label="Option 2" />
     <RadioButton value={3} label="Option 3" />
-  </Container>
+  </NormalStyles>
+));
+storiesOf('RadioButton', module).add('Inline', () => (
+  <InlineStyles>
+    <RadioButton inline value={1} label="Yes" checked />
+    <RadioButton inline value={2} label="No" />
+  </InlineStyles>
 ));
