a6b939e7278067fa6cbbcc8019803cacac973ebe
Christian Rodriguez <christian.rodriguez@capitalontap.com>
2019-07-30 10:14:04 +0100

    update progress bar to show without steps

--- src/components/ProgressBar/ProgressBar.stories.js
+++ src/components/ProgressBar/ProgressBar.stories.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import ProgressBar from "./ProgressBar";
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import ProgressBar from './ProgressBar';
 
-storiesOf("ProgressBar", module).add("Normal", () => (
-  <ProgressBar currentStep={5} progress={88} />
-));
+storiesOf('ProgressBar', module).add('Normal', () => <ProgressBar currentStep={5} progress={88} />);
+
+storiesOf('ProgressBar', module).add('No steps', () => <ProgressBar progress={77} noSteps />);
--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import Icon from "../atoms/Icon/Icon";
-import { colors as defaultColors } from "../../styles/defaults";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Icon from '../atoms/Icon/Icon';
+import { colors as defaultColors } from '../../styles/defaults';
 
 const Wrapper = styled.div`
   display: flex;
@@ -12,7 +12,7 @@ const Wrapper = styled.div`
 
 const Step = styled.div<{ width?: number }>`
   display: flex;
-  width: ${props => props.width + "%"};
+  width: ${props => props.width + '%'};
   justify-content: center;
   align-items: center;
   flex-direction: column;
@@ -21,58 +21,73 @@ const Step = styled.div<{ width?: number }>`
   }
 `;
 
-const BarWrapper = styled.div`
-  height: 4px;
+const BarWrapper = styled.div<{ noSteps?: boolean }>`
+  height: ${props => (props.noSteps ? '8px' : '4px')};
   margin-bottom: 20px;
-  overflow: hidden;
-  background-color: #fff;
+  overflow: ${props => (props.noSteps ? 'visible' : 'hidden')};
+  background-color: ${props => (props.noSteps ? props.theme.colorLightGrey : '#fff')};
   border-radius: 25px;
   width: 100%;
   margin-top: 1em;
 `;
 
-const Progress = styled.div<{ value?: number }>`
+const Progress = styled.div<{ value?: number; noSteps?: boolean }>`
   width: ${props => (props.value ? `${props.value}%` : 0)};
   height: 100%;
-  background-color: ${props =>
-    props.theme ? props.theme.colorPrimary : defaultColors.primary};
+  background-color: ${props => (props.theme ? props.theme.colorPrimary : defaultColors.primary)};
+  border-radius: ${props => (props.noSteps ? '25px' : '0')};
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 `;
 
 interface Props {
   steps: { icon: string; name: string }[];
   progress: number;
   currentStep: number;
+  noSteps: boolean;
+  data: { title: string; max: number | string; min: number; current: number | string };
 }
 
 const ProgressBar: React.SFC<Props> = props => {
-  const { steps, progress, currentStep } = props;
+  const { steps, progress, currentStep, noSteps, data } = props;
+
   return (
     <Wrapper {...props}>
-      {steps.map((step, index) => {
-        const iconName =
-          currentStep === index + 1 || index + 1 > currentStep
-            ? step.icon
-            : "check";
+      {!noSteps &&
+        steps.map((step, index) => {
+          const iconName =
+            currentStep === index + 1 || index + 1 > currentStep ? step.icon : 'check';
 
-        /**if brackground of icon should be solid/selected */
-        let isSolid = currentStep === index + 1;
+          /**if brackground of icon should be solid/selected */
+          let isSolid = currentStep === index + 1;
 
-        let isCompleted = currentStep > index + 1 ? true : false;
+          let isCompleted = currentStep > index + 1 ? true : false;
 
-        return (
-          <Step key={index} width={100 / steps.length}>
-            <Icon
-              name={iconName}
-              solid={isSolid}
-              primary={isCompleted}
-              circle
-            />
-            <span style={{ fontWeight: "bold" }}>{step.name}</span>
-          </Step>
-        );
-      })}
-      <BarWrapper>
-        <Progress value={progress} />
+          return (
+            <Step key={index} width={100 / steps.length}>
+              <Icon name={iconName} solid={isSolid} primary={isCompleted} circle />
+              <span style={{ fontWeight: 'bold' }}>{step.name}</span>
+            </Step>
+          );
+        })}
+      {noSteps && (
+        <TextWrapper>
+          <span>{data.title}</span>
+          <span>{data.current}</span>
+        </TextWrapper>
+      )}
+      <BarWrapper noSteps={noSteps}>
+        <Progress value={progress} noSteps={noSteps} />
+        {noSteps && (
+          <TextWrapper>
+            <span>{data.min}</span>
+            <span>{data.max}</span>
+          </TextWrapper>
+        )}
       </BarWrapper>
     </Wrapper>
   );
@@ -87,12 +102,13 @@ const ProgressBar: React.SFC<Props> = props => {
 (ProgressBar as any).defaultProps = {
   currentStep: 1,
   steps: [
-    { icon: "hourglass_empty", name: "Step 1" },
-    { icon: "person_outline", name: "Step 2" },
-    { icon: "person_outline", name: "Step 3" },
-    { icon: "credit_card", name: "Step 4" }
+    { icon: 'hourglass_empty', name: 'Step 1' },
+    { icon: 'person_outline', name: 'Step 2' },
+    { icon: 'person_outline', name: 'Step 3' },
+    { icon: 'credit_card', name: 'Step 4' }
   ],
-  progress: 75
+  progress: 75,
+  data: { title: 'Spending up-to-date:', max: '£5,000', min: '£0', current: '£3,850' }
 };
 
 export default ProgressBar;
