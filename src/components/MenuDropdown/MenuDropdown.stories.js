73ad0012368c2445d7975290159d78023702397f
christian-rodriguez-capontap <52253610+christian-rodriguez-capontap@users.noreply.github.com>
2020-04-14 21:21:00 +0100

    Christian/add menu item (#202)
    * v0.14.17
    
    * 0.14.18
    
    * 0.15.0
    
    * 1.0.0
    
    * 1.0.1
    
    * fixed version conflict
    
    * 1.0.0
    
    * update version
    
    * 1.0.0
    
    * 1.0.1
    
    * 2.0.0
    
    * 3.0.0
    
    * 3.1.0
    
    * 3.2.0
    
    * 3.3.0
    
    * 3.3.1
    
    * 3.3.2
    
    * 3.4.0
    
    * 4.0.0
    
    * 4.0.1-0
    
    * 4.0.2-0
    
    * 4.0.3-0
    
    * 0.14.34
    
    * 0.14.35
    
    * 0.14.42
    
    * add user nav to sidenav
    
    * update prop
    
    * update type issue

--- src/components/MenuDropdown/MenuDropdown.stories.js
+++ src/components/MenuDropdown/MenuDropdown.stories.js
@@ -1,8 +1,8 @@
-import { storiesOf } from "@storybook/react";
-import React from "react";
-import styled from "styled-components";
-import { colors as defaultColors } from "../../styles/defaults";
-import MenuDropdown from "./MenuDropdown";
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import styled from 'styled-components';
+import { colors as defaultColors } from '../../styles/defaults';
+import MenuDropdown from './MenuDropdown';
 
 const Container = styled.div`
   display: flex;
@@ -11,11 +11,11 @@ const Container = styled.div`
   background: ${defaultColors.default};
 `;
 
-storiesOf("MenuDropdown", module).add("Normal", () => (
+storiesOf('MenuDropdown', module).add('Normal', () => (
   <MenuDropdown firstName="Jamie" lastName="Howard" legalName="Potatoe Farm" locatorId="F415T98" />
 ));
 
-storiesOf("MenuDropdown", module).add("Small", () => (
+storiesOf('MenuDropdown', module).add('Small', () => (
   <MenuDropdown
     firstName="Jamie"
     lastName="Howard"
@@ -25,7 +25,7 @@ storiesOf("MenuDropdown", module).add("Small", () => (
   />
 ));
 
-storiesOf("MenuDropdown", module).add("Reversed normal", () => (
+storiesOf('MenuDropdown', module).add('Reversed normal', () => (
   <Container>
     <MenuDropdown
       firstName="Jamie"
@@ -37,7 +37,7 @@ storiesOf("MenuDropdown", module).add("Reversed normal", () => (
   </Container>
 ));
 
-storiesOf("MenuDropdown", module).add("Reversed small", () => (
+storiesOf('MenuDropdown', module).add('Reversed small', () => (
   <Container>
     <MenuDropdown
       firstName="Jamie"
@@ -50,7 +50,7 @@ storiesOf("MenuDropdown", module).add("Reversed small", () => (
   </Container>
 ));
 
-storiesOf("MenuDropdown", module).add("No details link", () => (
+storiesOf('MenuDropdown', module).add('No details link', () => (
   <MenuDropdown
     firstName="George Michael"
     lastName="Bluth"
@@ -60,7 +60,7 @@ storiesOf("MenuDropdown", module).add("No details link", () => (
   />
 ));
 
-storiesOf("MenuDropdown", module).add("Custom labels and link", () => (
+storiesOf('MenuDropdown', module).add('Custom labels and link', () => (
   <MenuDropdown
     firstName="George Michael"
     lastName="Bluth"
@@ -72,4 +72,38 @@ storiesOf("MenuDropdown", module).add("Custom labels and link", () => (
     logOutLabel="Exit"
     locatorId="AS12D34"
   />
+));
+
+storiesOf('MenuDropdown', module).add('Custom icon & Right nav popup ', () => (
+  <Container style={{ height: '500px' }}>
+    <MenuDropdown
+      firstName="George Michael"
+      lastName="Bluth"
+      legalName="Banana Stand"
+      showDetailLink={true}
+      customerReferenceLabel="Unique Identifier: "
+      detailLabel="Home"
+      detailUrl="https://www.capitalontap.com"
+      logOutLabel="Exit"
+      locatorId="AS12D34"
+      reverse
+      popupDirection="right"
+      renderLogo={() => (
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM10 3C11.66 3 13 4.34 13 6C13 7.66 11.66 9 10 9C8.34 9 7 7.66 7 6C7 4.34 8.34 3 10 3ZM4 13.98C5.29 15.92 7.5 17.2 10 17.2C12.5 17.2 14.71 15.92 16 13.98C15.97 11.99 11.99 10.9 10 10.9C8 10.9 4.03 11.99 4 13.98Z"
+            fill="white"
+          />
+        </svg>
+      )}
+    />
+  </Container>
 ));
