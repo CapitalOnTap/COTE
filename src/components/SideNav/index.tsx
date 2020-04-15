f9953e479f6fa1887b8368d6f42b52f91f114b2c
christian-rodriguez-capontap <52253610+christian-rodriguez-capontap@users.noreply.github.com>
2020-04-15 15:50:30 +0100

    Christian/add menu item (#203)
    * v0.14.17
    
    * 0.14.18
    
    * 0.15.0
    
    * 1.0.0
    
    * 1.0.1
    
    * fixed version conflict
    
    * 1.0.0
    
    * update version
    
    * 1.0.0
    
    * 1.0.1
    
    * 2.0.0
    
    * 3.0.0
    
    * 3.1.0
    
    * 3.2.0
    
    * 3.3.0
    
    * 3.3.1
    
    * 3.3.2
    
    * 3.4.0
    
    * 4.0.0
    
    * 4.0.1-0
    
    * 4.0.2-0
    
    * 4.0.3-0
    
    * 0.14.34
    
    * 0.14.35
    
    * 0.14.42
    
    * add user nav to sidenav
    
    * update prop
    
    * update type issue
    
    * update sidenav & menu dropdown

--- src/components/SideNav/index.tsx
+++ src/components/SideNav/index.tsx
@@ -5,7 +5,6 @@ import { colors as defaultColors } from '../../styles/defaults';
 import Icon from '../atoms/Icon/Icon';
 import OutsideAlerter from '../OutsideAlerter/OutsideAlerter';
 import { PaperWrapper } from '../Paper/Paper';
-import MenuDropdown from '../MenuDropdown/MenuDropdown';
 
 const Content = styled.div`
   display: flex;
@@ -78,7 +77,7 @@ const Link = styled.a`
 const Nav = styled(PaperWrapper.withComponent('nav'))<{
   width: number;
   isOpen?: boolean;
-  userNav?: boolean;
+  userNav?: () => void;
 }>`
   position: fixed;
   z-index: 10;
@@ -152,7 +151,7 @@ interface Props {
   isOpen?: boolean;
   iconsOnly?: boolean;
   renderLink?: (children: React.ReactNode, path: string) => React.ReactNode;
-  userNav?: boolean;
+  userNav?: () => React.ReactNode;
 }
 
 interface State {
@@ -245,40 +244,7 @@ class SideNav extends Component<Props, State> {
                 })}
               </List>
             </Content>
-            {userNav && (
-              <UserNavWrapper>
-                <MenuDropdown
-                  firstName="George Michael"
-                  lastName="Bluth"
-                  legalName="Banana Stand"
-                  showDetailLink={true}
-                  customerReferenceLabel="Unique Identifier: "
-                  detailLabel="Home"
-                  detailUrl="https://www.capitalontap.com"
-                  logOutLabel="Exit"
-                  locatorId="AS12D34"
-                  logOutClick={() => console.log('Logout!')}
-                  reverse
-                  popupDirection="right"
-                  renderLogo={() => (
-                    <svg
-                      width="20"
-                      height="20"
-                      viewBox="0 0 20 20"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM10 3C11.66 3 13 4.34 13 6C13 7.66 11.66 9 10 9C8.34 9 7 7.66 7 6C7 4.34 8.34 3 10 3ZM4 13.98C5.29 15.92 7.5 17.2 10 17.2C12.5 17.2 14.71 15.92 16 13.98C15.97 11.99 11.99 10.9 10 10.9C8 10.9 4.03 11.99 4 13.98Z"
-                        fill="white"
-                      />
-                    </svg>
-                  )}
-                />
-              </UserNavWrapper>
-            )}
+            {userNav && <UserNavWrapper>{userNav()}</UserNavWrapper>}
           </Nav>
         </OutsideAlerter>
         <Overlay isNavOpen={this.state.isOpen} />
@@ -308,7 +274,7 @@ class SideNav extends Component<Props, State> {
    * example: <Sidenav renderLink={(children, pathname) => <MyCustomLinkComponent route={pathname}>{children}</MyCustomLinkComponent>}
    */
   renderLink: PropTypes.func,
-  userNav: PropTypes.bool
+  userNav: PropTypes.func
 };
 
 (SideNav as any).defaultProps = {
@@ -357,7 +323,7 @@ class SideNav extends Component<Props, State> {
   iconsOnly: false,
 
   renderLink: undefined,
-  userNav: false
+  userNav: null
 };
 
 export default SideNav;
